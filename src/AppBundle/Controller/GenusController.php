b8e33deb3ad5b91c3be25ee22384bd96fc181d43
Edward Powderham <edpowderham@outlook.com>
2017-03-06 14:14:57 +0000

    fixing return template

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,8 +4,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
-
-//To use controller
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GenusController extends Controller
@@ -15,14 +13,10 @@ class GenusController extends Controller
   */
   public function showAction($genusName)
   {
-    //To get the templating service from the container - you need to give the name of the service.
     $templating = $this->container->get('templating');
-
-    //Defining the resposne object, rendered by the templating service with a defined path.
-    $html = $templating->render('genus/show.html.twig', [
+    //A shortcut for returning a template
+    return $this->render('genus/show.html.twig', [
       'name' => $genusName
     ]);
-
-    return new Response($html);
   }
 }
